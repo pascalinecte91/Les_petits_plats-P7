f9649666a0e54204e9f0e7824d391717f3407fc4
pascaline <pascalinecte@gmail.com>
2022-10-22 18:13:41 +0200

    🚑change name branch Algo2

--- script/service/SearchService.js
+++ script/service/SearchService.js
@@ -15,11 +15,11 @@ class SearchService {
 	launch() {
 		this.recipesRecovered = new Set();
 		this.searchParams = new SearchParams();
-
-		//debut de recherche
-		 //Array.from(this.recipes).map((recipe) => {
+		if (this.searchParams.input.length < 3) {
+      this.searchParams.input = "";
+		}
 		
-/* 		for (let recipe of [...this.recipes]) {
+	/* 	for (let recipe of [...this.recipes]) {
 			if (recipe.isValidSearchInput(this.searchParams.input) 
 			&& recipe.hasIngredients(this.searchParams.ingredients) 
 			&& recipe.hasUstensils(this.searchParams.ustensils) 
@@ -27,23 +27,27 @@ class SearchService {
 			) {
 				this.recipesRecovered.add(recipe);
 			} */
-			for(let i = 0; i < this.recipes.size ; i++) {
+			 for(let i = 0; i < this.recipes.size ; i++) {
 				let isRecovered =
 				this.recipes[i].isValidSearchInput(this.searchParams.input) &&
 				this.recipes[i].hasIngredients(this.searchParams.ingredients) &&
 				this.recipes[i].hasUstensils(this.searchParams.ustensils) &&
 				this.recipes[i].hasAppliances(this.searchParams.appliances);
-				if(isRecovered) {
+				if(this.isRecovered(recipe)) {
 					this.recipesRecovered.add(recipe);
 				}
-			}
-		};
+			 
+		}
 		// fin de recherche
 		this.searchResult = new SearchResult(this.recipesRecovered);
-		this.cardRecipeBuilder.refreshCardSection(this.searchResult.recipes);
-		this.listBuilder.refreshListSection(this.searchResult, this.searchParams);
-		this.alertMessage.refresh(this.searchResult);		
-		//console.log(this.searchResult,this.recipesRecovered);
-	}
-
+    this.cardRecipeBuilder.refreshCardSection(this.recipesRecovered);
+    this.listBuilder.refreshListSection(this.searchResult, this.searchParams);
+    this.alertMessage.refresh(this.searchResult);
+    if (this.recipesRecovered.size < this.recipes.size) {
+      document.getElementById("resultSort").classList.add("showInput");
+    } else {
+      document.getElementById("resultSort").classList.remove("showInput");
+    }
+  }
+}
 export default SearchService;
