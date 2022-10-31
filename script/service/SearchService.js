243d49da635f99087273c7620db5d7150d173451
pascaline <pascalinecte@gmail.com>
2022-10-31 17:02:56 +0100

    🚑 alglo3

--- script/service/SearchService.js
+++ script/service/SearchService.js
@@ -17,27 +17,19 @@ class SearchService {
 		this.recipesRecovered = new Set();
 		this.searchParams = new SearchParams();
 		if (this.searchParams.input.length < 3) {
-      this.searchParams.input = "";
+        this.searchParams.input = "";
 		}
 
-		for (let recipe of [...this.recipes]) {
-			if (recipe.isValidSearchInput(this.searchParams.input) 
-			&& recipe.hasIngredients(this.searchParams.ingredients) 
-			&& recipe.hasUstensils(this.searchParams.ustensils) 
-			&& recipe.hasAppliances(this.searchParams.appliances)
-			) {
-				this.recipesRecovered.add(recipe);
-			} 
-	/* 		 for(let i = 0; i < this.recipes.size ; i++) {
-				let isRecovered =
-				this.recipes[i].isValidSearchInput(this.searchParams.input) &&
-				this.recipes[i].hasIngredients(this.searchParams.ingredients) &&
-				this.recipes[i].hasUstensils(this.searchParams.ustensils) &&
-				this.recipes[i].hasAppliances(this.searchParams.appliances);
-				if(this.isRecovered(recipe)) {
-					this.recipesRecovered.add(recipe);
-				}
-		 */
+        //! size  sur un objet SET
+        for(let i = 0; i < this.recipes.size ; i++) {
+            let isRecovered =
+            this.recipes.isValidSearchInput(this.searchParams.input) &&
+            this.recipes.hasIngredients(this.searchParams.ingredients) &&
+            this.recipes.hasUstensils(this.searchParams.ustensils) &&
+            this.recipes.hasAppliances(this.searchParams.appliances);
+            if(this.isRecovered(recipe)) {
+                this.recipesRecovered.add(recipe);
+            }
 		}
 		// fin de recherche
 		this.searchResult = new SearchResult(this.recipesRecovered);
